6f63bfed24a66f26d0df7df178b14e3bd0154398
xyj <xyj@example.com>
2025-06-04 14:33:57 +0800

    Remove redundant code

--- pccontrol/layout/var/mobile/Library/ZXTouch/scripts/examples/Alert Box.bdl/alertbox.py
+++
@@ -1,5 +0,0 @@
-from zxtouch.client import zxtouch
-
-device = zxtouch("127.0.0.1")
-device.show_alert_box("ZXTouch Demo", "This is an AlertBox and will disappear in 3 seconds", 3)
-device.disconnect()
